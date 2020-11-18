83d232e01d1184bdb5aa9d13d2687ed994892fd9
Eduardo Bonilla <eduardobonillac@gmail.com>
2020-11-17 21:51:17 -0600

    bootstrap init

--- src/components/LeftNav.js
+++ src/components/LeftNav.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button'
 
 class LeftNav extends React.Component {
   render() {
+    return(
     <nav>
+      <h1 className="display-3">Hello</h1>
+      <Button variant="link">OK</Button>
     </nav>
+  )
   }
 }
 
